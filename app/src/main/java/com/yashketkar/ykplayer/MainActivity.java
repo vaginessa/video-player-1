088126842d7c75d46d98580a920883b9ca59b968
Yash Ketkar <yash.ketkar@cerner.com>
2018-01-15 17:33:06 -0600

    Update check for storage permission

--- app/src/main/java/com/yashketkar/ykplayer/MainActivity.java
+++ app/src/main/java/com/yashketkar/ykplayer/MainActivity.java
@@ -1,10 +1,8 @@
 package com.yashketkar.ykplayer;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,7 +17,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -31,30 +28,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, VideosFragment.OnFragmentInteractionListener, TorrentsFragment.OnFragmentInteractionListener, LiveTVFragment.OnFragmentInteractionListener {
 
-    private static final String PREF_USER_LEARNED_TORRENT = "torrent_learned";
-    private final static int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0;
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
     private Toolbar toolbar;
-    private String versioncode;
-    private String valert_title;
-    private String valert_message;
-    private String valert_message_big;
-    private String downloadurl;
     private boolean mUserLearnedTorrents;
+    private static final String PREF_USER_LEARNED_TORRENT = "torrent_learned";
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +66,7 @@ public class MainActivity extends AppCompatActivity
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,
                                                 int whichButton) {
-                                playvideo(nwinput.getText().toString()
+                                playVideo(nwinput.getText().toString()
                                         .replaceAll("[\\t\\n\\r]", ""));
                             }
                         });
@@ -105,45 +89,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                View fragmentContainerView = findViewById(R.id.fragment_container);
-                Snackbar.make(fragmentContainerView, "Storage access permission is required to scan and play media files on this device.", Snackbar.LENGTH_INDEFINITE)
-                        .setAction("OK", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-//                                ActivityCompat.requestPermissions((Activity) view.getContext(),
-//                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                            }
-                        })
-                        .show();
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-
-                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
         navigationView.setCheckedItem(R.id.nav_videos);
         getSupportActionBar().setTitle("Videos");
+
+        requestPermissions(MainActivity.this);
     }
 
     @Override
@@ -154,19 +103,16 @@ public class MainActivity extends AppCompatActivity
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     // permission was granted, yay! Do the
                     // videos-related task you need to do.
-                    switchfragments(VideosFragment.newInstance());
-
+                    switchFragment(VideosFragment.newInstance());
                 } else {
-
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    requestPermissions(MainActivity.this);
                 }
                 return;
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
         }
@@ -195,12 +141,10 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -212,13 +156,13 @@ public class MainActivity extends AppCompatActivity
         switch (id) {
             case R.id.nav_videos:
                 fragment = new VideosFragment().newInstance();
-                switchfragments(fragment);
+                switchFragment(fragment);
                 mTitle = "Videos";
                 restoreActionBar();
                 break;
             case R.id.nav_torrent_stream:
                 fragment = new TorrentsFragment().newInstance();
-                switchfragments(fragment);
+                switchFragment(fragment);
                 SharedPreferences sp = this.getSharedPreferences(
                         getString(R.string.preference_file_key), Context.MODE_PRIVATE);
                 mUserLearnedTorrents = sp.getBoolean(PREF_USER_LEARNED_TORRENT, false);
@@ -232,7 +176,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case R.id.nav_live_tv:
                 fragment = new LiveTVFragment().newInstance();
-                switchfragments(fragment);
+                switchFragment(fragment);
                 mTitle = "Live TV";
                 restoreActionBar();
                 break;
@@ -257,37 +201,71 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    public void onSectionAttached(int number) {
+        mTitle = getResources().getStringArray(R.array.nav_drawer_items)[number];
+    }
+
     public void onVideosFragmentInteraction(String id) {
-        playvideo(id);
+        playVideo(id);
     }
 
     public void onLiveTVFragmentInteraction(String id) {
-        playvideo(id);
+        playVideo(id);
     }
 
     public void onTorrentsFragmentInteraction(String id) {
-        playvideo(id);
-    }
-
-    public void onSectionAttached(int number) {
-        mTitle = getResources().getStringArray(R.array.nav_drawer_items)[number];
+        playVideo(id);
     }
 
     public void restoreActionBar() {
         toolbar.setTitle(mTitle);
     }
 
-    public void playvideo(String id) {
+    private void playVideo(String id) {
         Intent intent = new Intent(MainActivity.this,
                 VideoPlayerActivity.class);
         intent.putExtra("EXTRA_URL", id);
         startActivity(intent);
     }
 
-    public void switchfragments(Fragment fragment) {
+    private void switchFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+    private void requestPermissions(final Activity thisActivity) {
+        if (ContextCompat.checkSelfPermission(thisActivity,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                View fragmentContainerView = findViewById(R.id.fragment_container);
+                Snackbar.make(fragmentContainerView, "Storage access permission is required to scan and play media files on this device.", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(thisActivity,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                            }
+                        })
+                        .show();
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(thisActivity,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
